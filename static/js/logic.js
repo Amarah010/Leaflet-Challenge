b308e1f9ac7d4dc42cde878a9178b0bc3e2777a6
Amarah010 <ammaraha22@gmail.com>
2024-03-03 12:32:14 +0000

    completed files

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,15 +1,87 @@
 // Store url 
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Cretae initial map object
-var myMap = L.map("map", {
-    centre: [37.09, -95.71],
-    zoom: 5
-})
+// Perform a GET request to the query url
+d3.json(url).then(function (data) {
+    console.log(data)
+    createFeatures(data.features);
+  });
 
-// Add title layor to map
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
+  // Create marker size
+function markerSize(magnitude) {
+    return magnitude * 2000;
+  };
+  
+  // Create marker color by depth
+  function color(depth){
+    if (depth < 10) return "#00FF00";
+    else if (depth < 30) return "greenyellow";
+    else if (depth < 50) return "yellow";
+    else if (depth < 70) return "orange";
+    else if (depth < 90) return "orangered";
+    else return "#FF0000";
+  };
 
-// 
+function createFeatures(earthquakeData) {
+
+  // Define a function that we want to run once for each feature in the features array.
+  // Give each feature a popup that describes the place and time of the earthquake.
+  function onEachFeature(feature, layer) {
+    layer.bindPopup(`<h3>Location: ${feature.properties.place}</h3><hr><p>Date: ${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
+  };
+
+  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+  // Run the onEachFeature function once for each piece of data in the array.
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+    // Alter markers
+    pointToLayer: function(feature, latlng) {
+
+        // Style markers
+        var markers = {
+          radius: markerSize(feature.properties.mag),
+          fillColor: color(feature.geometry.coordinates[2]),
+          fillOpacity: 0.7,
+          color: "black",
+          stroke: true,
+          weight: 0.5
+        }
+        return L.circle(latlng,markers);
+      }
+  });
+  
+  createMap(earthquakes);
+};
+
+function createMap(earthquakes) {
+
+     // Create map 
+    var myMap = L.map("map", {
+    centre: [
+        37.09, -95.71
+    ],
+    zoom: 10,
+    });
+
+    // Create base layers
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(myMap);
+    
+    // Add a legend
+    var legend = L.control({position: "bottomright"});
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend"),
+    depth = [-10, 10, 30, 50, 70, 90];
+
+    div.innerHTML += "<h3 style='text-align: center'>Depth</h3>"
+
+    for (var i = 0; i < depth.length; i++) {
+      div.innerHTML +=
+      '<i style="background:' + color(depth[i] + 1) + '"></i> ' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+    }
+    return div;
+  };
+  legend.addTo(myMap)
+ 
+};
